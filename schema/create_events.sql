b543367e73be3902bf4ab6548b9b40ea5c8cf09a
arya237 <naderiarya20@gmail.com>
2025-02-10 15:40:18 +0330

    fix bug

--- schema/create_events.sql
+++ schema/create_events.sql
@@ -13,7 +13,8 @@ UPDATE PRODUCT
 JOIN ADDED_TO ON ADDED_TO.Product_ID = PRODUCT.ID
 JOIN LOCKED_SHOPPING_CART LSC ON ADDED_TO.ID = LSC.ID and ADDED_TO.Cart_number = LSC.Cart_number
      and LSC.Number = ADDED_TO.Locked_number
-LEFT JOIN ISSUED_FOR ISF ON LSC.ID = ISF.ID and LSC.Cart_number = ISF.Cart_number and LSC.Number = ISF.Locked_number  
+LEFT JOIN ISSUED_FOR ISF ON LSC.ID = ISF.ID and LSC.Cart_number = ISF.Cart_number and LSC.Number = ISF.Locked_number
+LEFT JOIN TRANSACTION T ON ISF.Tracking_code = T.Tracking_code  
 SET Stock_count = Stock_count + ADDED_TO.Quantity
-WHERE ISF.ID = NULL and LSC.Timestamp < NOW() - INTERVAL 3 DAY;
+WHERE (ISF.ID = NULL or T.Status = TRUE) and LSC.Timestamp < NOW() - INTERVAL 3 DAY);
 
