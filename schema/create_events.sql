b928b90b44f1a57e461a3b1e490debcf4cc2cff8
arya237 <naderiarya20@gmail.com>
2025-02-13 20:47:17 +0330

    fix bug

--- schema/create_events.sql
+++ schema/create_events.sql
@@ -9,14 +9,24 @@ WHERE Subcription_expiration_time < NOW() - INTERVAL 1 MONTH;
 CREATE EVENT IF NOT EXISTS check3DaysForSubmmitingLockedShoppingCart
 ON SCHEDULE EVERY 1 DAY
 DO
-UPDATE PRODUCT
-JOIN ADDED_TO ON ADDED_TO.Product_ID = PRODUCT.ID
-JOIN LOCKED_SHOPPING_CART LSC ON ADDED_TO.ID = LSC.ID and ADDED_TO.Cart_number = LSC.Cart_number
-     and LSC.Number = ADDED_TO.Locked_number
-LEFT JOIN ISSUED_FOR ISF ON LSC.ID = ISF.ID and LSC.Cart_number = ISF.Cart_number and LSC.Number = ISF.Locked_number
-LEFT JOIN TRANSACTION T ON ISF.Tracking_code = T.Tracking_code  
-SET Stock_count = Stock_count + ADDED_TO.Quantity
-WHERE (ISF.ID IS NULL or T.Status != TRUE) and LSC.Timestamp < NOW() - INTERVAL 3 DAY;
+
+     CREATE VIEW distinct_carts AS
+     SELECT Product_ID, SUM(Quantity) Quantity
+     FROM  (SELECT DISTINCT LSC.ID, LSC.Cart_number, LSC.Number, PRODUCT.ID Product_ID, ADDED_TO.Quantity
+     FROM PRODUCT JOIN ADDED_TO ON ADDED_TO.Product_ID = PRODUCT.ID
+     JOIN LOCKED_SHOPPING_CART LSC ON ADDED_TO.ID = LSC.ID and ADDED_TO.Cart_number = LSC.Cart_number
+          and LSC.Number = ADDED_TO.Locked_number 
+     JOIN SHOPPING_CART SH ON LSC.ID = SH.ID and LSC.Cart_number = SH.Number 
+     LEFT JOIN ISSUED_FOR ISF ON LSC.ID = ISF.ID and LSC.Cart_number = ISF.Cart_number 
+          and LSC.Number = ISF.Locked_number
+     LEFT JOIN TRANSACTION T ON ISF.Tracking_code = T.Tracking_code
+     WHERE SH.Status != 'active' and (ISF.ID IS NULL or T.Status != TRUE) )
+     AS distinct_carts GROUP BY Product_ID
+
+     UPDATE PRODUCT JOIN distinct_carts ON ID = Product_ID
+     SET Stock_count = Stock_count + Quantity;
+
+     DROP VIEW distinct_carts
 
 CREATE EVENT IF NOT EXISTS everyMonthBacking15PercentOfShoppingToVipClientsWallet
 ON SCHEDULE EVERY 1 MONTH
@@ -31,3 +41,16 @@ DO
      WHERE T.Status = TRUE;
 
      DROP VIEW vipClients;
+
+CREATE VIEW distinct_carts AS
+SELECT Product_ID, SUM(Quantity) Quantity
+FROM  (SELECT DISTINCT LSC.ID, LSC.Cart_number, LSC.Number, PRODUCT.ID Product_ID, ADDED_TO.Quantity
+FROM PRODUCT JOIN ADDED_TO ON ADDED_TO.Product_ID = PRODUCT.ID
+JOIN LOCKED_SHOPPING_CART LSC ON ADDED_TO.ID = LSC.ID and ADDED_TO.Cart_number = LSC.Cart_number
+     and LSC.Number = ADDED_TO.Locked_number 
+JOIN SHOPPING_CART SH ON LSC.ID = SH.ID and LSC.Cart_number = SH.Number 
+LEFT JOIN ISSUED_FOR ISF ON LSC.ID = ISF.ID and LSC.Cart_number = ISF.Cart_number 
+     and LSC.Number = ISF.Locked_number
+LEFT JOIN TRANSACTION T ON ISF.Tracking_code = T.Tracking_code
+WHERE SH.Status != 'active' and (ISF.ID IS NULL or T.Status != TRUE) )
+AS distinct_carts GROUP BY Product_ID
