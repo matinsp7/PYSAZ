c38639a60b317b75dab89360d6e64c138d0c6793
arya237 <naderiarya20@gmail.com>
2025-02-26 22:22:15 +0330

    link login page

--- PROJDB/frontend/homepage/static/homepage.js
+++ PROJDB/frontend/homepage/static/homepage.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const signUpLink = document.getElementById('signUpBtn');
+    const loginLink = document.getElementById("loginBtn")
 
     signUpLink.addEventListener('click', function (event) {
         event.preventDefault();
         window.location.href = "/signup";
     });
+
+    loginLink.addEventListener("click", function(event){
+        event.preventDefault()
+        window.location.href = "/login"
+    })
+
 });
